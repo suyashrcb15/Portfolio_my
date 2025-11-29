4af1a01ef73fed4fe79572c4453a8b90530cb311
Suyash Tiwari <142603779+suyashrcb15@users.noreply.github.com>
2025-11-29 12:21:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Navbar, Welcome} from "#components";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Welcome/>
+        </div>
+    )
+}
+export default App
+
